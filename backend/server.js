6dff7056f9eb1fdf1bc8b5fd2c4dc76d211aa3d7
Pravin Sarule <pravinsarule617@gmail.com>
2025-06-03 12:56:54 +0530

    backend update

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,26 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: 'https://vendor-project.netlify.app',
-  credentials: true, // if you want to allow cookies/auth headers
-}));
+// Express middleware
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'https://vendor-project.netlify.app',
+        'http://localhost:3000',
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 // Route Imports
 const authRoutes = require("./routes/authRoutes");
 
